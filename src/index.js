761b29af6942c27c2426a9627ebada158d9d3f9f
pacosanchezm <paco.sanchezm@gmail.com>
2022-05-18 16:17:35 -0500

    build3

--- src/index.js
+++ src/index.js
@@ -4,9 +4,13 @@ import ReactDOM from "react-dom"
 import App from "./appi.jsx"
 
 const rootElement = document.getElementById("root")
+
+
+
+
 ReactDOM.render(
   <React.StrictMode>
-    <App id={9998}/>
+    <App id={id}/>
     </React.StrictMode>,
   rootElement
 )
