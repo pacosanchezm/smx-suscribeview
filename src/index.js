ca20631260e61772f9bbac2228a197d3cedd1612
pacosanchezm <paco.sanchezm@gmail.com>
2022-07-07 12:20:15 -0500

    meh

--- src/index.js
+++ src/index.js
@@ -3,35 +3,10 @@ import ReactDOM from "react-dom"
 
 import App from "./appi.jsx"
 
-
-
-
 const rootElement = document.getElementById("root")
-
-
-
-window.attachApp = (usr, page, id, opt, token) => {
-
-  ReactDOM.render(
-    <React.StrictMode>
-      <App id={id}/>
-      </React.StrictMode>,
-    rootElement
-  )
-
-};
-
-
-// let id = 3544
-
-// ReactDOM.render(
-//   <React.StrictMode>
-//     <App id={id}/>
-//     </React.StrictMode>,
-//   rootElement
-// )
-
-
-
-
-
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+    </React.StrictMode>,
+  rootElement
+)
